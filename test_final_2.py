725cc3421b945c308d39e166c42c3d2f23c00240
burskyor <ludmila.rohac@gmail.com>
2024-09-14 15:22:27 +0200

    Add files via upload

---
+++ test_final_2.py
@@ -0,0 +1,29 @@
+
+# testování hlavního nadpisu stránky
+def test_busem_title(page):
+    page.goto("https://www.busem.cz/")
+    title = page.locator("title")
+    assert title.inner_text() == "Busem.cz | ČSAD Autobusy České Budějovice"
+
+# testování cookies
+def test_busem_cookies(page):
+    page.goto("https://www.busem.cz/")
+
+    button = page.locator(".iimGdprAcceptAll")
+    button.click()
+    
+    page.wait_for_selector("#iimGdprRoot", state="hidden")
+
+    div = page.locator("#iimGdprRoot")
+    assert div.is_visible() == False
+
+# testování vyhledávacího pole
+def test_submit_name(page):
+    page.goto("https://www.busem.cz/")
+    search = page.locator('input[name="search"][placeholder="Zadejte hledaný výraz"]').nth(0)
+    search.fill("rezervace jízdenek")
+   
+    assert page.title() == "Busem.cz | ČSAD Autobusy České Budějovice"
+
+    button = page.locator('button[type="submit"]')
+    button.click()
